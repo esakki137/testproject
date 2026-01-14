6dc6a44748ad8ef0a55bba0d720256e0a172a7f2
esakkimuthu <esakkimuthu.s@tech-swing.com>
2026-01-14 10:32:27 +0530

    Initial commit - Spring Boot application

---
+++ src/main/java/com/spr/app/SpringController/TestController.java
@@ -0,0 +1,17 @@
+package com.spr.app.SpringController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+	@GetMapping("hello")
+	public String sayHello() {
+		System.out.println("Request arrived to say hello!");
+		return "Hi there, This is a test hello call.";
+	}
+}
